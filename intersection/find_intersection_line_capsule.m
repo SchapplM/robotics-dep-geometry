65d3f75d3710e99b69a2a617f08ef8b8d8e2e47c
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-05-16 09:50:02 +0100

    Zeichensatz auf UTF8 geändert

--- intersection/find_intersection_line_capsule.m
+++ intersection/find_intersection_line_capsule.m
@@ -16,14 +16,18 @@
 % pts [3x2]
 %   intersection points, or nearest point and distance to nearest point
 %   stacked with NaNs, when no intersections exist
+
+% Jonathan Vorndamme, vorndamme@irt.uni-hannover.de, 2016-06
+% (C) Institut f√ºr Regelungstechnik, Leibniz Universit√§t Hannover
+
 function pts = find_intersection_line_capsule(p, u, p1, p2, r)
-  % Berechne Lotfuﬂpunkt q der windschiefen geraden g1: x=p+s*u; s aus R und
+  % Berechne Lotfu√üpunkt q der windschiefen geraden g1: x=p+s*u; s aus R und
   % g2: x=p1+t*v; v=p2-p1, t aus R auf der geraden g1. Dazu wird die
-  % Hilfsebene E: x=p1+s*v+t*(u x v) konstruiert, die g2 enth‰lt und als
-  % zweite Richtung die senkrechte zu beiden geraden (u x v) enth‰lt. Der
+  % Hilfsebene E: x=p1+s*v+t*(u x v) konstruiert, die g2 enth√§lt und als
+  % zweite Richtung die senkrechte zu beiden geraden (u x v) enth√§lt. Der
   % normalenvektor von E lautet demnach v x (u x v). Die Normalengleichung
   % ergibt sich als (x-p1)*(v x (u x v))=0. Setzt man die geradengleichung
-  % von g1 hier ein, kann man den parameter s f¸r den Lotfuﬂpunkt
+  % von g1 hier ein, kann man den parameter s f√ºr den Lotfu√üpunkt
   % bestimmen: (p+s*u-p1)*(v x (u x v))=0
   % <=> s*u*(v x (u x v))=(p1-p)*(v x (u x v))
   % <=> s=((p1-p)*(v x (u x v)))/(u*(v x (u x v)))
@@ -41,29 +45,29 @@ function pts = find_intersection_line_capsule(p, u, p1, p2, r)
   % <=> s^2*(u x v)^2+2*s*(u x v)*((p-p1) x v)+((p-p1) x v)^2-r^2*v^2=0
   % <=> s = -(u x v)*((p-p1) x v)/(u x v)^2+-sqrt((u x v)*((p-p1) x v)^2/(u x v)^4-((((p-p1) x v)^2-r^2*v^2)/(u x v)^2).
   % Liegen keine Schnittpunkte mit diesem vor (s nicht reell), so wird der
-  % n‰chstgelegene Punkt zur Kapsel bestimmt. Wenn Schnittpunkte vorliegen,
-  % wird gepr¸ft, ob Sie auf dem zur Kapsel gehˆrigen Teil des unendlichen
+  % n√§chstgelegene Punkt zur Kapsel bestimmt. Wenn Schnittpunkte vorliegen,
+  % wird gepr√ºft, ob Sie auf dem zur Kapsel geh√∂rigen Teil des unendlichen
   % Zylinders liegen. Das ist der Fall, wenn (q-p2)*v<0 und
   % (q-p1)*v>0 ist. Ist die erste Bedingung verletzt, gibt es
   % (mindestens) eine Schnittpunkt mit der Halbkugel um p1, ist die zweite
   % Bedingung verletzt, gibt es (mindestens) eine Schnittpunkt mit der
   % Halbkugel um p2. Dies folgt daraus, dass die Skalarprodukte die
   % relative Richtung der Projektion des Vektors von pi nach q auf den
-  % Vektor von p1 nach p2 angibt. F¸r die beiden Kugeln wird, falls
+  % Vektor von p1 nach p2 angibt. F√ºr die beiden Kugeln wird, falls
   % notwendig der Schnittpunkt nach folgendem Ansatz berechnet:
   % (p-p1+s*u)^2 = r^2 <=> s^2*u^2+2*(p-p1)*u*s+(p-p1)^2-r^2=0
   % <=> s=-(p-p1)*u/u^2+-sqrt(((p-p1)*u)^2/u^4-((p-p1)^2-r^2)/u^2)
-  % f¸r die Halbkugel um p1 und
+  % f√ºr die Halbkugel um p1 und
   % s=-(p-p2)*u/u^2+-sqrt(((p-p2)*u)^2/u^4-((p-p2)^2-r^2)/u^2)
-  % f¸r die Halbkugel um p2. Danach muss noch gepr¸ft werden (wieder ¸ber
+  % f√ºr die Halbkugel um p2. Danach muss noch gepr√ºft werden (wieder √ºber
   % obiges Skalarprodukt), ob die gefundenen Schnittpunkte in der korrekten
-  % H‰lfte der Kugel liegen.
+  % H√§lfte der Kugel liegen.
   
-  % Hilfsfunktion zur Korrektheitspr¸fung und Korrektur der
+  % Hilfsfunktion zur Korrektheitspr√ºfung und Korrektur der
   % Zylinderschnittpunkte an der Seite p1
   function p_out = correct_intersection_k1
     sqrt_s = sqrt(((p-p1).'*u)^2/(u.'*u)^2-((p-p1).'*(p-p1)-r^2)/(u.'*u));
-    if imag(sqrt_s)~=0 % falls keine Schnittpunkte vorliegen: n‰chster pkt
+    if imag(sqrt_s)~=0 % falls keine Schnittpunkte vorliegen: n√§chster pkt
       p_out = find_next_pkt;
       pts = [p_out [norm(cross(p_out-p,u))/norm(u); NaN(2,1)]];
       finished=1;
@@ -74,23 +78,23 @@ function pts = find_intersection_line_capsule(p, u, p1, p2, r)
     s2 = s_s-sqrt_s;
     pc11 = p+s1*u;
     pc12 = p+s2*u;
-    if (pc11-p1).'*v<0 % pc11 auf richtiger H‰lfte
-      if (pc12-p1).'*v<0 % pc12 auf richtiger H‰lfte
+    if (pc11-p1).'*v<0 % pc11 auf richtiger H√§lfte
+      if (pc12-p1).'*v<0 % pc12 auf richtiger H√§lfte
         pts=[pc11 pc12];
         finished=1;
         p_out=0;
         return;
       end
       p_out=pc11;
-    elseif (pc12-p1).'*v<0 % pc12 auf richtiger H‰lfte
+    elseif (pc12-p1).'*v<0 % pc12 auf richtiger H√§lfte
       p_out=pc12;
     end
   end
-  % Hilfsfunktion zur Korrektheitspr¸fung und Korrektur der
+  % Hilfsfunktion zur Korrektheitspr√ºfung und Korrektur der
   % Zylinderschnittpunkte an der Seite p2
   function p_out = correct_intersection_k2
     sqrt_s = sqrt(((p-p2).'*u)^2/(u.'*u)^2-((p-p2).'*(p-p2)-r^2)/(u.'*u));
-    if imag(sqrt_s)~=0 % falls keine Schnittpunkte vorliegen: n‰chster pkt
+    if imag(sqrt_s)~=0 % falls keine Schnittpunkte vorliegen: n√§chster pkt
       p_out = find_next_pkt;
       pts = [p_out [norm(cross(p_out-p,u))/norm(u); NaN(2,1)]];
       finished=1;
@@ -101,15 +105,15 @@ function pts = find_intersection_line_capsule(p, u, p1, p2, r)
     s2 = s_s-sqrt_s;
     pc11 = p+s1*u;
     pc12 = p+s2*u;
-    if (pc11-p2).'*v>0 % pc11 auf richtiger H‰lfte
-      if (pc12-p2).'*v>0 % pc12 auf richtiger H‰lfte
+    if (pc11-p2).'*v>0 % pc11 auf richtiger H√§lfte
+      if (pc12-p2).'*v>0 % pc12 auf richtiger H√§lfte
         pts=[pc11 pc12];
         finished=1;
         p_out=0;
         return;
       end
       p_out=pc11;
-    elseif (pc12-p2).'*v>0 % pc12 auf richtiger H‰lfte
+    elseif (pc12-p2).'*v>0 % pc12 auf richtiger H√§lfte
       p_out=pc12;
     end
   end
@@ -117,19 +121,19 @@ function pts = find_intersection_line_capsule(p, u, p1, p2, r)
   cpp1v  = cross(p-p1,v);
   sqrt_z = sqrt((cuv.'*cpp1v)^2/(cuv.'*cuv)^2-(cpp1v.'*cpp1v-r^2*(v.'*v))/(cuv.'*cuv));
   % Wenn keine Schnittpunkte vorliegen, muss der Punkt auf der Kapsel
-  % berechnet werden, der der Geraden g1 am n‰chsten liegt. Dazu werden
-  % wieder die drei F‰lle "q jenseits von p1", "q jenseits von p2" und "q
+  % berechnet werden, der der Geraden g1 am n√§chsten liegt. Dazu werden
+  % wieder die drei F√§lle "q jenseits von p1", "q jenseits von p2" und "q
   % zwischen p1 und p2" unterschieden. Fall 1 und 2: Addiert man zu p die
-  % Projektion von des Vektors von p nach pi auf u, so erh‰lt man den pi
-  % n‰chstgelegenden Punkt qi auf der Geraden g1. Geht man von pi in Richtung
+  % Projektion von des Vektors von p nach pi auf u, so erh√§lt man den pi
+  % n√§chstgelegenden Punkt qi auf der Geraden g1. Geht man von pi in Richtung
   % dieses Punktes (Weite r), so landet man am gesuchten Punkt p_c:
   % qi = p+(pi-p)*u/u^2*u, p_c = pi+(qi-pi)/|qi-pi|*r
-  % Fall 3: wir kennen die Richtung von q nach g2: u x v. Den Lotfuﬂpunkt
-  % q3 auf g2 kˆnnen wir analog zu Fall 1/2 berechnen:
-  % q3 = p1+(q-p1)*v/v^2*v von da aus gehen wir um die L‰nge r in
+  % Fall 3: wir kennen die Richtung von q nach g2: u x v. Den Lotfu√üpunkt
+  % q3 auf g2 k√∂nnen wir analog zu Fall 1/2 berechnen:
+  % q3 = p1+(q-p1)*v/v^2*v von da aus gehen wir um die L√§nge r in
   % Richtung (q-q3), um den gesuchten Punkt zu finden.
-  % Hilfsfunktion zum Finden des n‰chstgelegenen Punktes auf der
-  % Kapseloberfl‰che
+  % Hilfsfunktion zum Finden des n√§chstgelegenen Punktes auf der
+  % Kapseloberfl√§che
   function p_out = find_next_pkt
     if (q-p1).'*v<0
       q1  = p+(p1-p).'*u/(u.'*u)*u;
@@ -143,10 +147,10 @@ function pts = find_intersection_line_capsule(p, u, p1, p2, r)
     end
     p_out=p_c;
   end
-  if (imag(sqrt_z)~=0) % kein Schnittpunkt liegt vor, w‰hle n‰chsten Punkt
+  if (imag(sqrt_z)~=0) % kein Schnittpunkt liegt vor, w√§hle n√§chsten Punkt
     pc1 = find_next_pkt;
     pts = [pc1, [norm(cross(pc1-p,u))/norm(u); NaN(2,1)]];
-  else % Schnittpunkte gefunden, auf Korrektheit pr¸fen und ggf. korrigieren
+  else % Schnittpunkte gefunden, auf Korrektheit pr√ºfen und ggf. korrigieren
     s_z = -cuv.'*cpp1v/(cuv.'*cuv);
     s1  = s_z+sqrt_z;
     s2  = s_z-sqrt_z;
